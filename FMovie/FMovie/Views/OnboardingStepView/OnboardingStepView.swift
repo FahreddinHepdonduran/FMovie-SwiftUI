b65a1e14e4c9c12b6dfed9252a754a615bab0a23
Fahreddin Hepdonduran <fh@Fahreddins-Mac-mini.local>
2023-05-18 22:27:09 +0300

    complete onboarding scene

--- FMovie/FMovie/Views/OnboardingStepView/OnboardingStepView.swift
+++ FMovie/FMovie/Views/OnboardingStepView/OnboardingStepView.swift
@@ -11,6 +11,9 @@ struct OnboardingStepView: View {
     
     var title: String = ""
     var imageName: String = ""
+    var isLast: Bool = false
+    var currentIndex: Int = 0
+    var continueAction: ((_ index: Int) -> Void)?
     
     var body: some View {
         ZStack {
@@ -26,29 +29,64 @@ struct OnboardingStepView: View {
             }
             
             VStack {
-                Spacer(minLength: 300.0)
+                Spacer(minLength: 350.0)
                 
                 Image("marvelLogo")
                 
                 Spacer()
                 
-                Text(title)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                    .padding([.leading, .trailing], 90.0)
-                
-                Spacer()
-                
-                Button(action: {
-                    print("continue")
-                }) {
-                    Text("Continue")
-                        .frame(width: 300.0, height: 50.0, alignment: .center)
+                HStack(spacing: 10.0) {
+                    ForEach(0..<6) { index in
+                        Circle()
+                            .foregroundColor(index == currentIndex ? .red : .white)
+                            .frame(width: 10.0, height: 10.0)
+                    }
                 }
-                .background(Color.red)
-                .foregroundColor(Color.white)
                 
                 Spacer()
+                
+                if isLast {
+                    VStack(spacing: 20.0) {
+                        Button(action: {
+                            print("Signup")
+                        }) {
+                            Text("Signup")
+                                .frame(width: 300.0, height: 50.0, alignment: .center)
+                        }
+                        .background(Color.red)
+                        .foregroundColor(Color.white)
+                        
+                        Button(action: {
+                            print("Login")
+                        }) {
+                            Text("Login")
+                                .frame(width: 300.0, height: 50.0, alignment: .center)
+                        }
+                        .background(Color.clear)
+                        .foregroundColor(Color.white)
+                        .border(Color.red, width: 3.0)
+                    }
+                    
+                    Spacer()
+                } else {
+                    Text(title)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding([.leading, .trailing], 90.0)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        continueAction?(currentIndex + 1)
+                    }) {
+                        Text("Continue")
+                            .frame(width: 300.0, height: 50.0, alignment: .center)
+                    }
+                    .background(Color.red)
+                    .foregroundColor(Color.white)
+                    
+                    Spacer()
+                }
             }
         }
     }
--- FMovie/FMovie/Views/OnboardingView/OnboardingView.swift
+++ FMovie/FMovie/Views/OnboardingView/OnboardingView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    
+    @State private var selectedTabIndex: Int = 0
+    
+    private let onboardingData: [(String, String)] = [
+        ("Step1", "onboarding1"),
+        ("Step2", "onboarding2"),
+        ("Step3", "onboarding3"),
+        ("Step4", "onboarding4"),
+        ("Step5", "onboarding5"),
+        ("Step6", "onboarding6")
+    ]
+    
     var body: some View {
-        Text("Hello")
+        if #available(iOS 14.0, *) {
+            TabView(selection: $selectedTabIndex) {
+                ForEach(Array(onboardingData.enumerated()), id: \.1.0) { index, item in
+                    OnboardingStepView(
+                        title: item.0,
+                        imageName: item.1,
+                        isLast: index == onboardingData.count - 1,
+                        currentIndex: index,
+                        continueAction: { index in
+                            selectedTabIndex = index
+                        }
+                    )
+                    .tag(index)
+                }
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .edgesIgnoringSafeArea(.all)
+        } else {
+            // TODO: Custom page view for earlier versions
+        }
     }
+    
 }
 
 struct OnboardingView_Previews: PreviewProvider {
